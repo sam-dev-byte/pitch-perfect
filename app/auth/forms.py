1dc3e8a43ad74cf20d08d7a25cca2dfd5780744d
Gift kipkemboi <mokayasam3@gmail.com>
2022-02-11 14:20:31 -0500

    created my html pages

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -11,7 +11,10 @@ class RegistrationForm(FlaskForm):
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
 
-    
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
